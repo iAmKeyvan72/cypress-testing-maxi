56a76cac8707f7c0e097ceb621fcbaa103a3d195
Keyvan Mahmoudi <5063636+iAmKeyvan72@users.noreply.github.com>
2023-03-10 21:59:08 +0100

    finish section 3

--- cypress/e2e/contact.cy.ts
+++ cypress/e2e/contact.cy.ts
@@ -3,18 +3,43 @@ describe('contact form', () => {
     cy.visit('http://localhost:5174/about');
     cy.get('[data-cy="contact-input-message"]').type('Hello Human!');
     cy.get('[data-cy="contact-input-name"]').type('K1');
-    cy.get('[data-cy="contact-input-email"]').type('k1@mahmoudi.com');
     cy.get('[data-cy="contact-btn-submit"]').as('submitBtn');
     cy.get('@submitBtn').then((el) => {
       expect(el.attr('disbaled')).to.be.undefined;
       expect(el.text()).to.be.equal('Send Message');
     });
-    // cy.get('@submitBtn').contains(/send message/i);
-    // cy.get('@submitBtn').should('not.have.attr', 'disabled');
-    cy.get('@submitBtn').click();
+    cy.get('[data-cy="contact-input-email"]').type('k1@mahmoudi.com{enter}');
+
     cy.get('@submitBtn').should('have.attr', 'disabled');
     cy.get('@submitBtn').contains(/sending.../i);
     cy.get('@submitBtn').should('not.have.attr', 'disabled');
     cy.get('@submitBtn').contains(/send message/i);
+  });
+
+  it('should validate form', () => {
+    cy.visit('http://localhost:5174/about');
+    cy.get('[data-cy="contact-btn-submit"]').as('submitBtn');
+    cy.get('@submitBtn').click();
+    cy.get('@submitBtn').should('not.have.attr', 'disabled');
+    cy.get('@submitBtn').should('not.contain', 'Sending...');
+    cy.get('@submitBtn').contains('Send Message');
+
+    cy.get('[data-cy="contact-input-message"]').focus().blur();
+    cy.get('[data-cy="contact-input-message"]')
+      .parent()
+      .should('have.attr', 'class')
+      .and('match', /invalid/);
+
+    cy.get('[data-cy="contact-input-name"]').focus().blur();
+    cy.get('[data-cy="contact-input-name"]')
+      .parent()
+      .should('have.attr', 'class')
+      .and('match', /invalid/);
+
+    cy.get('[data-cy="contact-input-email"]').focus().blur();
+    cy.get('[data-cy="contact-input-email"]')
+      .parent()
+      .should('have.attr', 'class')
+      .and('match', /invalid/);
   });
 });
