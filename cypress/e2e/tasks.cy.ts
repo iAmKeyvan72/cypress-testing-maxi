638de1c7b65c554f4032c7595457c60c2b1e490d
Keyvan Mahmoudi <5063636+iAmKeyvan72@users.noreply.github.com>
2023-03-10 15:39:31 +0100

    test filteres

--- cypress/e2e/tasks.cy.ts
+++ cypress/e2e/tasks.cy.ts
@@ -25,4 +25,29 @@ describe('tasks management', () => {
       .and('contain.html', 'h2')
       .and('contain.text', 'kosshere toolani');
   });
+
+  it('should validate task before adding', () => {
+    cy.visit('http://localhost:5173');
+    cy.get('button').contains('Add Task').click();
+    cy.get('dialog.modal button').contains('Add Task').click();
+    cy.get('.error-message').should('contain.text', 'Please provide');
+  });
+
+  it('should filter the tasks', () => {
+    cy.visit('http://localhost:5173');
+    cy.get('button').contains('Add Task').click();
+    cy.get('select#category').select('important');
+    cy.get('dialog.modal button').contains('Add Task').click();
+    cy.get('.error-message').should('contain.text', 'Please provide values');
+    cy.get('input#title').type('kossher');
+    cy.get('textarea#summary').type('kossere toolani.');
+    cy.get('dialog.modal button').contains('Add Task').click();
+    cy.get('li.task').should('have.length', 1).and('contain.text', 'kossher');
+    cy.get('select#filter').select('urgent');
+    cy.get('li.task').should('have.length', 0);
+    cy.get('select#filter').select('important');
+    cy.get('li.task').should('have.length', 1).and('contain.text', 'kosser');
+    cy.get('select#filter').select('all');
+    cy.get('li.task').should('have.length', 1);
+  });
 });
