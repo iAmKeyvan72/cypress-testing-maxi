c1208a5a4f7691be3f42ea189873971cf98a749a
Keyvan Mahmoudi <5063636+iAmKeyvan72@users.noreply.github.com>
2023-03-13 19:31:17 +0100

    add fixtures

--- cypress/e2e/location.cy.ts
+++ cypress/e2e/location.cy.ts
@@ -1,22 +1,39 @@
 describe('share location', () => {
-  it('should fetch the user location', () => {
+  beforeEach(() => {
+    cy.fixture('user-location.json').as('userLocation');
     cy.visit('/').then((win) => {
-      cy.stub(win.navigator.geolocation, 'getCurrentPosition')
-        .as('getUserLocation')
-        .callsFake((cb) => {
-          setTimeout(() => {
-            cb({
-              coords: {
-                latitude: 37.3,
-                longitude: 29.53,
-              },
-            });
-          }, 100);
-        });
+      cy.get('@userLocation').then((fakeLocation) => {
+        cy.stub(win.navigator.geolocation, 'getCurrentPosition')
+          .as('getUserLocation')
+          .callsFake((cb) => {
+            setTimeout(() => {
+              cb(fakeLocation);
+            }, 100);
+          });
+      });
+      cy.stub(win.navigator.clipboard, 'writeText')
+        .as('copyLocation')
+        .resolves();
     });
+  });
+  it('should fetch the user location', () => {
     cy.get('[data-cy="get-loc-btn"]').click();
     cy.get('@getUserLocation').should('have.been.called');
     cy.get('[data-cy="get-loc-btn"').should('be.disabled');
     cy.get('[data-cy="actions"').should('contain.text', 'Location fetched');
+  });
+
+  it('should share the location', () => {
+    cy.get('[data-cy="name-input"]').type('kire khar');
+    cy.get('[data-cy="get-loc-btn"]').click();
+    cy.get('[data-cy="share-loc-btn"]').click();
+    cy.get('@copyLocation').should('have.been.called');
+    cy.get('@userLocation').then((fakeLocation: any) => {
+      const { latitude, longitude } = fakeLocation.coords;
+      cy.get('@copyLocation').should(
+        'be.calledWithMatch',
+        new RegExp(`${latitude}.*${longitude}.*${encodeURI('kire khar')}`)
+      );
+    });
   });
 });
