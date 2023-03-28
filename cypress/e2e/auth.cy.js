19245a0e6a8b3b1256676912de56ed8e7b15e504
Keyvan Mahmoudi <5063636+iAmKeyvan72@users.noreply.github.com>
2023-03-28 11:38:54 +0200

    it should login

--- cypress/e2e/auth.cy.js
+++ cypress/e2e/auth.cy.js
@@ -14,4 +14,15 @@ describe('auth', () => {
     cy.location('pathname').should('equal', '/takeaways');
     cy.getCookie('__session').its('value').should('not.be.empty');
   });
+
+  it('should login', () => {
+    cy.visit('/login');
+    cy.get('[data-cy="auth-email"]').click();
+    cy.get('[data-cy="auth-email"]').type('test@example.com');
+    cy.get('[data-cy="auth-password"]').click();
+    cy.get('[data-cy="auth-password"]').type('testpassword');
+    cy.get('[data-cy="auth-submit"]').click();
+    cy.location('pathname').should('equal', '/takeaways');
+    cy.getCookie('__session').its('value').should('not.be.empty');
+  });
 });
